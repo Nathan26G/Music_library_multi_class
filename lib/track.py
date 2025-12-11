de401c3373867e8c537437cf35bff50283a799d0
Nathan Gallagher <nathangallagher101@gmail.com>
2025-12-11 11:29:34 +0000

    track test driven

--- lib/track.py
+++ lib/track.py
@@ -1,20 +1,12 @@
-from lib.music_library import *
-
 class Track:
     # Public properties:
     #   title: a string
     #   artist: a string
 
     def __init__(self, title, artist):
-        # Parameters:
-        #   title: a string
-        #   artist: a string
-        # Side-effects:
-        #   Sets the title and artist properties
-        pass
+        self.title = title
+        self.artist = artist
 
     def format(self):
-        # Returns:
-        #   a string in the format "TITLE by ARTIST"
-        pass
+        return f'{self.title} By {self.artist}'
 
--- test/test_track.py
+++ test/test_track.py
@@ -1,1 +1,14 @@
 from lib.track import *
+
+def test_title_is_set_when_initalised():
+    track = Track('Teddy Picker', 'Arctic Monkeys')
+    assert track.title == 'Teddy Picker'
+    
+def test_artist_is_set_when_initalised():
+    track = Track('Teddy Picker', 'Arctic Monkeys')
+    assert track.artist == 'Arctic Monkeys'
+    
+def test_format_returns_correct_string():
+    track = Track('Go With The Flow', 'Queens of the Stone Age')
+    formated = track.format()
+    assert formated == 'Go With The Flow By Queens of the Stone Age'
